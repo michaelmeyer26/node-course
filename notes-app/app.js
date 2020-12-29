c304e1b9b3acb825b98d7a8806d346da55475db8
Michael <michael.c.meyer26@gmail.com>
2020-12-29 15:19:15 -0600

    set up add command options

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,59 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
+//customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note...')
-} else if (command === 'remove') {
-    console.log('Removing note...')
-}
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log("Title: " + argv.title);
+        console.log("Body: " + argv.body);
+    }
+});
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note...');
+    }
+});
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler: function() {
+        console.log('Reading the note...');
+    }
+});
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Lists out all the currently-saved notes',
+    handler: function() {
+        console.log('Here is a list of all the notes...');
+    }
+});
+
+// add, remove, read, list
+
+yargs.parse();
