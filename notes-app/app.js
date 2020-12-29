951ebc91a539e783abef55ce06469fb7eb88bf5c
Michael <michael.c.meyer26@gmail.com>
2020-12-29 13:03:03 -0600

    finished chalk exercise

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,9 @@
-const validator = require('validator');
+// const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const notes = getNotes();
 console.log(notes);
 
-console.log(validator.isURL('https://mead.io'));
+// console.log(validator.isURL('https://mead.io'));
+console.log(chalk.green.inverse.bold('Success!'));
