8be92a5f7da0330bd46781a5cfb605c901a896dc
Michael <michael.c.meyer26@gmail.com>
2020-12-29 14:47:13 -0600

    using process.argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,10 @@
-// const validator = require('validator');
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const notes = getNotes();
-console.log(notes);
+const command = process.argv[2];
 
-// console.log(validator.isURL('https://mead.io'));
-console.log(chalk.green.inverse.bold('Success!'));
+if (command === 'add') {
+    console.log('Adding note...')
+} else if (command === 'remove') {
+    console.log('Removing note...')
+}
