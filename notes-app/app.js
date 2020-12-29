dc2dc80f298b1d5a1163de99b9026b9e41477d62
Michael <michael.c.meyer26@gmail.com>
2020-12-28 18:56:33 -0600

    first challenge complete! Initial fs usage

---
+++ notes-app/app.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'My name is Michael.');
+
+fs.appendFileSync('notes.txt', ' I am learning Node.js in addition to learning Java through a coding bootcamp, Codeup.');
