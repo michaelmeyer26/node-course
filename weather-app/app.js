23a49bd18c5acd4e1b5cf904cda163aa24779c55
Michael <michael.c.meyer26@gmail.com>
2021-01-28 23:38:01 -0600

    added error handling for mapbox call

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 
-const weatherURL = 'http://api.weatherstack.com/current?access_key=2e28a3938e25d2304efdf71fdebcc9a6&query=&units=f';
+const weatherURL = 'http://api.weatherstack.com/current?access_key=2e28a3938e25d2304efdf71fdebcc9a6&query=30.2711,-97.7437&units=f';
 
 request({ url: weatherURL, json: true }, (error, response) => {
     if (error) {
@@ -12,11 +12,16 @@ request({ url: weatherURL, json: true }, (error, response) => {
     }
 })
 
-// const geocodeURL = 'http://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWljaGFlbGNtZXllcjI2IiwiYSI6ImNrZWltM2hqeTFmcjgzM3VxcGt0cGkxbnYifQ.Nd_zH244hFL7-jClbxQj8Q&limit=1';
+const geocodeURL = 'http://api.mapbox.com/geocoding/v5/mapbox.places/Austin.json?access_token=pk.eyJ1IjoibWljaGFlbGNtZXllcjI2IiwiYSI6ImNrZWltM2hqeTFmcjgzM3VxcGt0cGkxbnYifQ.Nd_zH244hFL7-jClbxQj8Q&limit=1';
 
-// request({ url: geocodeURL, json: true}, (error, response) => {
-//     // console.log(response.body);
-//     const latitude = response.body.features[0].center[1];
-//     const longitude = response.body.features[0].center[0];
-//     console.log('lat: ' + latitude + '\nlng: ' + longitude)
-// })
+request({ url: geocodeURL, json: true}, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to location service')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location, please check search term and try again')
+    } else {
+        const latitude = response.body.features[0].center[1];
+        const longitude = response.body.features[0].center[0];
+        console.log('lat: ' + latitude + '\nlng: ' + longitude)
+    }
+})
