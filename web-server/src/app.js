486e2f645ae327cde37c9d1611061c5334ea37cd
Michael <michael.c.meyer26@gmail.com>
2021-02-06 14:12:08 -0600

    Added a generic 404 view and a couple of pages to utilize it

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,23 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, '../templates');
 
+// Define Paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Setup handlebars and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -28,16 +36,33 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: "Help Page",
-        message: "Help message?"
+        title: "Help",
+        name: "Michael Meyer",
+        helpMessage: "Help message"
     })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
         "Location": "Philadeplhia",
-        "Forecast": "Always Sunny"
+        "Forecast": "Sunny"
     });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: "Help",
+        name: "Michael Meyer",
+        errorMessage: "Help article not found!"
+    })
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: "Help",
+        name: "Michael Meyer",
+        errorMessage: "Uh oh! Page not found!"
+    })
 });
 
 //app.com
