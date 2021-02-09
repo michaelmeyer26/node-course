3053992e3062bd64a746ee7932ee83de3ae7bd14
Michael <michael.c.meyer26@gmail.com>
2021-02-09 17:05:42 -0600

    set up /weather to accept a query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -43,9 +43,29 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address.'
+        })
+    }
+
     res.send({
-        "Location": "Philadeplhia",
-        "Forecast": "Sunny"
+        "location": "Philadelphia",
+        "forecast": "Sunny",
+        "address": req.query.address
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term.'
+        })
+    }
+
+    console.log(req.query);
+    res.send({
+        products: []
     });
 });
 
