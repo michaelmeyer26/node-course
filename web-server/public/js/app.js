65b0248e9f49509fab32373020142ccaed486fd6
Michael <michael.c.meyer26@gmail.com>
2021-02-12 15:25:20 -0600

    Setting up fetch call to retrieve data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side JavaScript file is loaded!');
+
+fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        }
+
+        console.log('Location: ', data.location);
+        console.log('Forecast: ', data.forecast);
+    });
+});
