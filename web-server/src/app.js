35cde2398157649683e99dad4a387ccbb90add5d
Michael <michael.c.meyer26@gmail.com>
2021-02-11 23:32:00 -0600

    played around with function default parameters

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -53,12 +53,12 @@ app.get('/weather', (req, res) => {
 
     geocode(req.query.address, (error, { latitude, longitude, location} = {}) => {
         if (error) {
-            return console.log('Error: ', error);
+            return res.send({ error });
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return console.log('Error: ', error);
+                return res.send({ error });
             }
 
             res.send({
