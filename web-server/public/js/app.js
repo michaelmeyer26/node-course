a506760484ee6448aa5fa723aa820d34e9b1ba46
Michael <michael.c.meyer26@gmail.com>
2021-02-17 18:37:58 -0600

    Created a search form and tied the fetch to a submit

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,23 @@
 console.log('Client side JavaScript file is loaded!');
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
-        console.log('Location: ', data.location);
-        console.log('Forecast: ', data.forecast);
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            }
+
+            console.log('Location: ', data.location);
+            console.log('Forecast: ', data.forecast);
+        });
     });
 });
