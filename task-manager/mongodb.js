4ec1b20fbc95b2d725a3ecb0c840e2ecf5f0b3f3
Michael <michael.c.meyer26@gmail.com>
2021-02-20 17:04:16 -0600

    Learned more about the functions and methods and created a new collection while inserting tasks

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,8 +13,52 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Michael',
-        age: 26
+    // db.collection('users').insertOne({
+    //     name: 'Michael',
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: "Applied to jobs",
+            completed: false
+        },
+        {
+            description: "Learned more Node.JS",
+            completed: true
+        },
+        {
+            description: "Studied for interview",
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.ops)
     })
 })
