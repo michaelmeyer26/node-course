809302450e8aabac817779239ebe98582c1c1919
Michael <michael.c.meyer26@gmail.com>
2021-03-17 13:56:30 -0500

    update second example to use async/await

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose.js')
 const Task = require('../src/models/task.js')
 
-Task.findByIdAndDelete('604ad35e86a20242d0807b64').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('604ad35e86a20242d0807b64').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+
+    return count
+}
+
+deleteTaskAndCount('604ad31357a0544278560f36').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
