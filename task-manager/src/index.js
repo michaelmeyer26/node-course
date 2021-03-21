7698ef51e9f5d5f4a0ed5faaea1a4360d0a29bad
Michael <michael.c.meyer26@gmail.com>
2021-03-20 20:24:21 -0500

    delete endpoint for tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -143,6 +143,22 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
