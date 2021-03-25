f71958e416e2d54e4043424d607606dec9d08538
Michael <michael.c.meyer26@gmail.com>
2021-03-25 16:00:31 -0500

    Adding password hashing into the routes thru the use of middleware and some refactoring

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim: true
-    }, email: {
+    }, 
+    email: {
         type: String,
         required: true,
         trim: true,
@@ -16,7 +18,8 @@ const User = mongoose.model('User', {
                 throw new Error('Email is invalid')
             }
         }
-    }, age: {
+    }, 
+    age: {
         type: Number,
         default: 0,
         validate(value) {
@@ -24,7 +27,8 @@ const User = mongoose.model('User', {
                 throw new Error('Age must be a positive number')
             }
         }
-    }, password: {
+    }, 
+    password: {
         type: String,
         trim: true,
         required: true,
@@ -37,5 +41,17 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
