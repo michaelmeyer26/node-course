0742d1bb6ddbdf126e7c7c2cdbbae261e071cbf3
Michael <michael.c.meyer26@gmail.com>
2021-03-26 16:40:38 -0500

    refactor task route to allow middleware

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const Task = require('../models/task')
+const User = require('../models/user')
 
 router.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -49,7 +50,11 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try{
-        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        const task = await Task.findById(_id)
+
+        updates.forEach((update) => task[update] = req.body[update])
+
+        await task.save()
 
         if (!task) {
             return res.status(404).send()
