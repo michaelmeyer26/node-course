03dfb925e50ea435001ecb334673dd9a93e1919a
Michael <michael.c.meyer26@gmail.com>
2021-04-15 12:56:40 -0500

    adding pagination options

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,6 +20,7 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 //GET /tasks?completed=false
+//GET /tasks?limit=10&skip=0
 router.get('/tasks', auth, async (req, res) => {
     const match = {  }
     
@@ -32,7 +33,11 @@ router.get('/tasks', auth, async (req, res) => {
         // const tasks = await Task.find({ owner: req.user._id })
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
