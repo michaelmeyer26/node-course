48af3d72ba1e6cbd683c3dbf598941e3c0fa8b5e
Michael <michael.c.meyer26@gmail.com>
2021-04-22 23:12:56 -0500

    added validation to avatar upload route

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -4,7 +4,17 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match( /\.(jpeg|jpg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 router.post('/users', async (req, res) => {
