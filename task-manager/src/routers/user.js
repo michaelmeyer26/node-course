b89e12f2693e0a80de0b2248ed4a129364907169
Michael <michael.c.meyer26@gmail.com>
2021-04-24 22:11:48 -0500

    refactored user model and avatar route to attach image to user

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -4,7 +4,6 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -96,7 +95,9 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
