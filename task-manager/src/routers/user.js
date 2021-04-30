b3ed9dc268a0180ddaf212853ecf9999a3c30a52
Michael <michael.c.meyer26@gmail.com>
2021-04-29 23:08:01 -0500

    sharp is broken and idk why

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
