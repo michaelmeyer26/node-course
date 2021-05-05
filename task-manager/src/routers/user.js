417072f8eb3a98ef6205ab63bb0bb379a8ebc064
Michael <michael.c.meyer26@gmail.com>
2021-05-05 13:46:56 -0500

    Added welcome email functionality but can no longer create new users

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail } = require('../emails/account')
 const multer = require('multer')
 const upload = multer({
     limits: {
@@ -22,6 +23,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
