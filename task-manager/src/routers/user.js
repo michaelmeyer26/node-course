29c31c40db6c4f166302399fb38e185f167562ec
Michael <michael.c.meyer26@gmail.com>
2021-05-06 17:22:58 -0500

    Added cancellation email to delete route

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,7 +3,7 @@ const router = new express.Router()
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
-const { sendWelcomeEmail } = require('../emails/account')
+const { sendWelcomeEmail, sendCancellationEmail } = require('../emails/account')
 const multer = require('multer')
 const upload = multer({
     limits: {
@@ -92,6 +92,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendCancellationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
