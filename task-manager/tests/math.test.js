641133c295759c6b2517c1ded994eb5ae9fadb06
Michael <michael.c.meyer26@gmail.com>
2021-05-10 20:42:01 -0500

    further testing jest tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,15 @@
-test('Hello World', () => {
+const { calculateTip } = require('../src/math')
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
+
+    // if (total !== 13) {
+    //     throw new Error('Total tip should be 13, got ' + total)
+    // }
 })
 
-test('This should fail', () => {
-    throw new Error('Failure')
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12)
 })
